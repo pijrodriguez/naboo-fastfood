a9e11eb7a263d547b6263d1ab10313257bd17d13
A00999371 <mskerl@my.bcit.ca>
2017-05-04 12:27:05 -0700

    message for people ro read

---
+++ index.js
@@ -0,0 +1,57 @@
+/**********************************CONSTS & VARS*************************************/
+const NEWPORT = process.env.PORT || 10000;
+
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const express = require("express");
+const path = require("path");
+const pg = require("pg");
+
+var CLF = path.resolve(__dirname, "pages");
+var app = express();
+var io = require("socket.io")(sv);
+const sv = require("http").createServer(app);
+
+io.on("connection", function(socket){
+    socket.on("disconnect", function(){
+    });
+});
+
+var dbURL = process.env.DATABASE_URL || //"postgres://postgres:"Password"@localhost:5432/"database"";
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use("/js", express.static("build"));
+
+app.use(session({
+    secret:"welcome to naboo",
+    resave:true,
+    saveUninitialized:true
+}));
+
+/**********************************ROOT FOLDERS*************************************/
+app.get("/", function(req, resp){
+    resp.sendFile(CLF+"/login-page.html");
+});
+app.get("/kitchen-page", function(req, resp){
+    resp.sendFile(CLF+"/kitchen-page.html");
+});
+app.get("/admin-page", function(req, resp){
+    resp.sendFile(CLF+"/admin-page.html");
+});
+app.get("/main-page", function(req, resp){
+    resp.sendFile(CLF+"/main.html");
+});
+app.get("/order-page", function(req, resp){
+    resp.sendFile(CLF+"/order-page.html");
+});
+
+/**********************************LISTEN*************************************/
+sv.listen(NEWPORT, function(err){
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log(NEWPORT+" is running");
+});

4a3b1f89ec502fb00cf309cdad1302f9347b436e
Mark Skerl <mskerl@my.bcit.ca>
2017-05-08 10:25:57 -0700

    login page functional
    Signed-off-by: Mark Skerl <mskerl@my.bcit.ca>

--- index.js
+++ index.js
@@ -9,20 +9,21 @@ const pg = require("pg");
 
 var CLF = path.resolve(__dirname, "pages");
 var app = express();
-var io = require("socket.io")(sv);
 const sv = require("http").createServer(app);
+var io = require("socket.io")(sv);
+
 
 io.on("connection", function(socket){
     socket.on("disconnect", function(){
     });
 });
 
-var dbURL = process.env.DATABASE_URL || //"postgres://postgres:"Password"@localhost:5432/"database"";
+var dbURL = process.env.DATABASE_URL || //"postgres://postgres:PASSWORD@localhost:5432/naboo";
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use("/js", express.static("build"));
+app.use("/scripts", express.static("build"));
 
 app.use(session({
     secret:"welcome to naboo",
@@ -54,4 +55,41 @@ sv.listen(NEWPORT, function(err){
         return false;
     }
     console.log(NEWPORT+" is running");
+});
+
+/**********************************LOGIN*************************************/
+app.post("/accountLOGIN", function(req, resp){
+    pg.connect(dbURL, function(err, client, done){
+        if(err){
+            console.log(err);
+            resp.send({status:"fail"});
+        }
+        
+        client.query("SELECT id, emp_id, type, name, password FROM users WHERE emp_id = $1 AND password = $2", [req.body.d_empId, req.body.d_password], function(err, result){
+            
+            done();
+            if(err){
+                console.log(err);
+                resp.send({status:"fail"});
+            }
+            
+            if(result.rows.length > 0){
+                req.session.user = result.rows[0];
+                if(result.rows[0].type=="a"){
+                    resp.send({status:"success", user:req.session.user});
+                }
+                if(result.rows[0].type=="c"){
+                    resp.send({status:"success2", user:req.session.user});
+                }
+            } else {
+                resp.send({status:"fail"});
+            }
+        });
+    })
+});
+
+/**********************************LOGOUT*************************************/
+app.post("/logout", function(req, resp){
+    req.session.destroy();
+    resp.end("success");
 });

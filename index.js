9c49d4f10f4e07dbae53d46b81093ec3258848a3
pijrodriguez <pijrodriguez@yahoo.com.ph>
2017-05-23 16:33:24 -0700

    Finalized the admin page

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ io.on("connection", function(socket){
     });
 });
 
-var dbURL = process.env.DATABASE_URL || "postgres://postgres:PASSWORD@localhost:5432/naboo";
+var dbURL = process.env.DATABASE_URL || "postgres://postgres:Jelapij8@localhost:5432/naboo";
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -374,8 +374,8 @@ app.post("/close-store", function(req, resp){
         resp.end("Success");
     })
 })
-/**********************************SALES*************************************/
-app.post("/get-sales", function(req, resp){
+/**********************************ITEMS SOLD*************************************/
+app.post("/get-items-sold", function(req, resp){
     pg.connect(dbURL, function (err, client, done) {
         if (err) {
             console.log(err);
@@ -415,11 +415,53 @@ app.post("/get-dates", function(req, resp){
     })
 })
 
+/**********************************SALES*************************************/
+app.post("/get-dates-sales", function(req, resp){
+    pg.connect(dbURL, function (err, client, done) {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+
+        client.query("SELECT DISTINCT date FROM orders", function(err,result){
+            done();
+            if(err){
+                return false;
+            }
+            resp.send({
+                status: "Success",
+                dates: result.rows
+            });
+        })
+    })
+})
+
+app.post("/get-sales", function(req, resp){
+    pg.connect(dbURL, function (err, client, done) {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+
+        client.query("SELECT * FROM orders WHERE date = $1", [req.body.date_selected], function(err,result){
+            done();
+            if(err){
+                return false;
+            }
+            resp.send({
+                status: "Success",
+                sales: result.rows
+            });
+        })
+    })
+})
+
 /**********************************SEND USER INFO*************************************/
 app.post("/get-user", function(req, resp){
     resp.send({
         status: "Success",
-        user: JSON.stringify(req.session.user)
+        user: JSON.stringify(req.session.user),
+        store: storeIsOpen
     })
 });
 
--- js/login-page.js
+++ js/login-page.js
@@ -6,30 +6,56 @@ $(document).ready(function(){
 /**********************************VARS*************************************/
     var loginBut = document.getElementById("loginButton"),
         passInput = document.getElementById("passWord"),
-        empInput =document.getElementById("empId");
+        empInput =document.getElementById("empId"),
+        regExEmpId = /^[A][0-9][0-9][0-9]{1,3}$/,
+        regExPassword = /^[a-zA-Z0-9]{5,50}$/;
     
 /**********************************LOGIN*************************************/
+    empInput.onkeyup = function(){
+        if(!regExEmpId.test(empInput.value)){
+            empInput.style.boxShadow = '0 0 35px 1px red';
+            empInput.style.borderBottom = 'red';
+        } else{
+            empInput.style.boxShadow = '0 0 35px 1px limegreen';
+            empInput.style.borderBottom = 'green';
+        }
+    }
+
+    passInput.onkeyup = function () {
+        if(!regExPassword.test(passInput.value)){
+            passInput.style.boxShadow = '0 0 35px 1px red';
+            passInput.style.borderBottom = 'red';
+        } else{
+            passInput.style.boxShadow = '0 0 35px 1px limegreen';
+            passInput.style.borderBottom = 'green';
+        }
+    }
+
     loginBut.addEventListener("click", function(){
         console.log("Attempting to login");
-        $.ajax({
-            url:"/accountLOGIN",
-            type:"post",
-            data:{
-                d_password: passInput.value,
-                d_empId: empInput.value
-            },
-            success:function(resp){
-                console.log(resp);
-                if(resp.status == "success"){
-                    location.href = "/admin-page";
+        if(!regExEmpId.test(empInput.value)||!regExPassword.test(passInput.value)){
+            alert("Please make sure all inputs are valid.")
+        } else{
+            $.ajax({
+                url:"/accountLOGIN",
+                type:"post",
+                data:{
+                    d_password: passInput.value,
+                    d_empId: empInput.value
+                },
+                success:function(resp){
+                    console.log(resp);
+                    if(resp.status == "success"){
+                        location.href = "/admin-page";
+                    }
+                    else if(resp.status == "success2"){
+                        location.href = "/kitchen-page";
+                    }
+                    else {
+                        alert("Incorrect Employee ID or password");
+                    }
                 }
-                else if(resp.status == "success2"){
-                    location.href = "/kitchen-page";
-                }
-                else {
-                    alert("Incorrect Employee ID or password");
-                }
-            }
-        }); 
+            });
+        }
     });
 })
